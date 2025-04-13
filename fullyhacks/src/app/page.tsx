1818f3eddbeca6cd2805497f2ae4e7a90d682962
Ryan Wagner <rjw@csu.fullerton.edu>
2025-04-13 00:32:49 -0700

    test stars animation

--- fullyhacks/src/app/page.tsx
+++ fullyhacks/src/app/page.tsx
@@ -1,11 +1,39 @@
 import './page.css'
-const MainPage = () => {
+/*const MainPage = () => {
 
   return (
+    
     <div className='center'>
       
       <div className="box">Hello</div>
     </div>
+  );
+};*/
+const MainPage = () => {
+  return (
+    <>
+      <header id="header">
+        <a id="logo">navbar</a>
+        <nav id="nav">
+          <button id="btn">
+            <span id="hamburguer">
+              <span></span>
+            </span>
+          </button>
+          <ul id="menu">
+            <li><a href="/">about</a></li>
+            <li><a href="/">skills</a></li>
+            <li><a href="/">works</a></li>
+          </ul>
+        </nav>
+      </header>
+
+      <section className="wrapper">
+        <div id="stars"></div>
+        <div id="stars2"></div>
+        <div id="stars3"></div>
+      </section>
+    </>
   );
 };
 
